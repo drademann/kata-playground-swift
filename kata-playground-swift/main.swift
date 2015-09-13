24d44db4a1a7876f73fc5bcc86f264f76bbcb15a
Dirk Rademann <contact@beyondworlds.net>
2015-09-13 12:47:17 +0200

    Initial Commit

---
+++ kata-playground-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  kata-playground-swift
+//
+//  Created by Dirk Rademann on 13.09.15.
+//  Copyright © 2015 beyondworlds.net. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
