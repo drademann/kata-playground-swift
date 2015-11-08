52599fa1c7ba4a0659ff710c4dc44a3ca7b64ac2
Dirk Rademann <contact@beyondworlds.net>
2015-11-08 09:43:21 +0100

    Renamed to better readability.

--- kata-playground-swift/stringCalculator.swift
+++ kata-playground-swift/stringCalculator.swift
@@ -4,16 +4,16 @@ struct StringCalculator {
 	
 	func sum() -> Int {
 		if hasInput() {
-			return sum(asInt(splitted()))
+			return sum(ofInts(ofSplittedInput()))
 		}
 		return 0;
 	}
 
 	private func hasInput() -> Bool { return !input.isEmpty }
 
-	private func splitted() -> [String] { return input.characters.split(",").map(String.init)	}
+	private func ofSplittedInput() -> [String] { return input.characters.split(",").map(String.init)	}
 	
-	private func asInt(strings: [String]) -> [Int] { return strings.map { Int($0)! }}
+	private func ofInts(strings: [String]) -> [Int] { return strings.map { Int($0)! }}
 	
 	private func sum(numbers: [Int]) -> Int { return numbers.reduce(0, combine: { $0 + $1 })}
 }
